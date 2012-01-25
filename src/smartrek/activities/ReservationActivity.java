44a86c827b287b07d86aa16b300cd00c57545864
Sumin Byeon <suminb@gmail.com>
2012-01-25 02:39:21 -0700

    Displaying text elements with ListActivity.

--- src/smartrek/activities/ReservationActivity.java
+++ src/smartrek/activities/ReservationActivity.java
@@ -1,12 +1,17 @@
 package smartrek.activities;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class ReservationActivity extends Activity {
+public class ReservationActivity extends ListActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.reservation);
+        //setContentView(R.layout.reservation);
+        
+        String[] items = new String[] { "alpha", "beta", "charlie" };
+        
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, items));
 	}
 }
