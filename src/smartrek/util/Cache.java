47578ab9b7b1ccad6ec9cd59b328a96ed9c5c9a6
Sumin Byeon <suminb@gmail.com>
2012-03-14 02:45:50 -0700

    Naive caching mechanism is in place (unstable).

--- src/smartrek/util/Cache.java
+++ src/smartrek/util/Cache.java
@@ -1,12 +1,14 @@
 package smartrek.util;
 
+import java.io.IOException;
 import java.util.Hashtable;
 
 import android.text.format.Time;
 
 /**
  * This class contains key-value pairs where key is a remote URL and a value
- * is a collection of the content and the metadata.
+ * is a collection of the content and the metadata. Also, the caches are
+ * intended to stay in memory (no use of persistent storage).
  *
  * TODO: We need some sort of garbage collection mechanism to clear off old
  *       data from 'storage'. 
@@ -57,19 +59,48 @@ public final class Cache {
 		Time currentTime = new Time();
 		currentTime.setToNow();
 		
-		return data != null && Time.compare(currentTime, data.expires) > 0;
+		return data != null && Time.compare(data.expires, currentTime) > 0;
 	}
 	
-	public Object fetch(String url) {
+	public Object fetch(String url) throws IOException {
 		if(storage.containsKey(url)) {
 			Data data = storage.get(url);
 			
 			if(isValid(data)) {
 				return data.userdata;
 			}
+			else {
+				// TODO: Clear off old data
+			}
+		}
+		else {
+			// TODO: Fetch and cache
+			
+			HTTP http = new HTTP(url);
+			http.connect();
+			
+			int code = http.getResponseCode();
+			if(code == 200) {
+				// HTTP OK
+				
+				String body = http.getResponseBody();
+				
+				Data data = new Data();
+				Time expire = new Time();
+				expire.setToNow();
+				expire.set(expire.toMillis(false) + TTL*1000);
+				data.expires = expire;
+				data.userdata = body;
+				
+				storage.put(url, data);
+				
+				return data.userdata;
+			}
+			else {
+				// TODO: Deal with error
+			}
 		}
 		
-		// TODO: Fetch and cache
 		return null;
 	}
 }
