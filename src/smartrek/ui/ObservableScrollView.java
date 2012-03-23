12c45938affcbbbfd8e6b4b5db219f155744700f
Sumin Byeon <suminb@gmail.com>
2012-03-23 16:36:02 -0700

    Implemented observable scroll view.

---
+++ src/smartrek/ui/ObservableScrollView.java
@@ -0,0 +1,27 @@
+package smartrek.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+
+public class ObservableScrollView extends HorizontalScrollView {
+
+	private ScrollViewListener scrollViewListener = null;
+
+    public ObservableScrollView(Context context, AttributeSet attributes) {
+    	super(context, attributes);
+    }
+
+    public void setScrollViewListener(ScrollViewListener scrollViewListener) {
+        this.scrollViewListener = scrollViewListener;
+    }
+
+    @Override
+    protected void onScrollChanged(int x, int y, int oldx, int oldy) {
+        super.onScrollChanged(x, y, oldx, oldy);
+        if(scrollViewListener != null) {
+            scrollViewListener.onScrollChanged(this, x, y, oldx, oldy);
+        }
+    }
+}
---
+++ src/smartrek/ui/ScrollViewListener.java
@@ -0,0 +1,8 @@
+package smartrek.ui;
+
+
+public interface ScrollViewListener {
+
+    void onScrollChanged(ObservableScrollView scrollView, int x, int y, int oldx, int oldy);
+
+}
