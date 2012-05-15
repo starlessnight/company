78d373745ef6dc633bd19988ac644f6ac4feb35a
Sumin Byeon <suminb@gmail.com>
2012-05-14 20:37:52 -0700

    Revised deploy script.

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ FULL_REV=$(git rev-parse HEAD)
 SHORT_REV=${FULL_REV:0:8}
 
 scp bin/SmarTrek.apk static.suminb.com:webapps/static/smartrek/smartrek-${SHORT_REV}.apk
-echo "http://static.suminb.com/smartrek?rev=${SHORT_REV}"
+echo "http://static.suminb.com/smartrek/?rev=${SHORT_REV}"
