1f8277a244defad6aaf9d041e5d63ff409013fcd
Sumin Byeon <suminb@gmail.com>
2012-05-31 17:50:32 -0700

    K-d tree building mechanism seems to work.

--- src/com/smartrek/utils/KdTree.java
+++ src/com/smartrek/utils/KdTree.java
@@ -2,6 +2,8 @@ package com.smartrek.utils;
 
 import java.util.List;
 
+import android.util.Log;
+
 /**
  * 
  *
@@ -17,19 +19,21 @@ public class KdTree {
 	}
 	
 	/**
-	 * Builds a K-d tree for a given list of nodes
+	 * Builds a K-d tree for a given list of nodes.
+	 * NOTE: 'nodes' must be in order.
 	 * 
 	 * @param nodes
 	 * @param l Left boundary of nodes (inclusive)
-	 * @param r Right boundary of nodes (exclusive)
+	 * @param r Right boundary of nodes (inclusive)
 	 * @param depth
 	 * @return
 	 */
 	public static Node build(List<RouteNode> nodes, int l, int r, int depth) {
 		
-		// FIXME: This function causes stack overflow
+	    Log.d("KdTree", String.format("left=%d, right=%d, depth=%d", l, r, depth));
 		
-		if (l >= r - 1) {
+		if (l > r || depth >= 10) {
+		    Log.d("KdTree", "Return");
 			return null;
 		}
 		
@@ -47,19 +51,29 @@ public class KdTree {
     node.right_child = kdtree(point_list[median + 1:], depth + 1)
 		 */
 		
-		int median = (r - l) / 2;
+		int median = (r + l) / 2;
 		
 		Node node = new Node();
 		node.routeNode = nodes.get(median);
-		node.left = build(nodes, l, median, depth + 1);
-		node.right = build(nodes, median, r, depth + 1);
+		node.left = build(nodes, l, median-1, depth + 1);
+		node.right = build(nodes, median+1, r, depth + 1);
 		
 		return node;
 	}
 	
+	public static void print(Node node, int depth) {
+	    String padding = "";
+        for (int i=0; i<depth; i++)
+            padding += "  ";
+        Log.d("KdTree", String.format("%s%f, %f", padding, node.routeNode.getLatitude(), node.routeNode.getLongitude()));
+        
+        if (node.left != null)
+            print(node.left, depth+1);
+        if (node.right != null)
+            print(node.right, depth+1);
+    }
+	
 	public KdTree() {
 		
 	}
-	
-	// TODO: Implement a function to visualize the tree
 }
