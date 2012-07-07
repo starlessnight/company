e786f194540e603468d7832fd8fe0d93e4599981
Sumin Byeon <suminb@gmail.com>
2012-07-06 17:03:42 -0700

    Exception handling service.

---
+++ src/com/smartrek/utils/ExceptionHandlingService.java
@@ -0,0 +1,54 @@
+package com.smartrek.utils;
+
+import java.io.PrintStream;
+import java.util.Stack;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+public class ExceptionHandlingService {
+    private Stack<Exception> exceptions = new Stack<Exception>();
+    private Context context;
+    
+    public ExceptionHandlingService(Context context) {
+        this.context = context;
+    }
+    
+    public synchronized boolean hasExceptions() {
+        return !exceptions.isEmpty();
+    }
+    
+    public synchronized void registerException(Exception e) {
+        registerException(e, System.err);
+    }
+    
+    public synchronized void registerException(Exception e, PrintStream err) {
+        if (err != null) {
+            e.printStackTrace(err);
+        }
+        exceptions.push(e);
+    }
+    
+    public synchronized void reportException(String message) {
+        AlertDialog dialog = new AlertDialog.Builder(context).create();
+        dialog.setTitle("Exception");
+        dialog.setMessage(message);
+        dialog.setButton("Dismiss", new Dialog.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        dialog.show();
+    }
+    
+    public synchronized void reportExceptions() {
+        while (!exceptions.isEmpty()) {
+            Exception e = exceptions.pop();
+            
+            reportException(e.getMessage());
+        }
+    }
+}
