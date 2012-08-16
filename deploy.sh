f47347dfcf83b84b82d0883ed3ed2065524a47ef
Sumin Byeon <suminb@gmail.com>
2012-08-15 17:56:55 -0700

    Uses 12-character prefix for commit IDs instead of 8.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FULL_REV=$(git rev-parse HEAD)
-SHORT_REV=${FULL_REV:0:8}
+SHORT_REV=${FULL_REV:0:12}
 
 scp bin/smartrek-android.apk static.suminb.com:webapps/static/smartrek/smartrek-${SHORT_REV}.apk
 echo "http://static.suminb.com/smartrek/?rev=${SHORT_REV}"
