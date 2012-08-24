81aad7b7561ea3e3fe5986fef1f901dcd985d0a1
Sumin Byeon <suminb@gmail.com>
2012-08-24 01:22:09 -0700

    Design proposal.

--- src/com/smartrek/requests/FavoriteAddressAddRequest.java
+++ src/com/smartrek/requests/FavoriteAddressAddRequest.java
@@ -5,11 +5,22 @@ import java.net.URLEncoder;
 
 public class FavoriteAddressAddRequest extends Request {
 	
+	public FavoriteAddressAddRequest(int uid, String name, String address) {
+		// TODO: Implement this		
+	}
+	
+	/**
+	 * @deprecated
+	 */
 	public void execute(int uid, String name, String address) throws IOException {
 		String url = String.format("%s/addfavadd/?UID=%d&NAME=%s&ADDRESS=%s",
 				HOST, uid, URLEncoder.encode(name), URLEncoder.encode(address));
 		
 		executeHttpGetRequest(url);
+	}
+	
+	public void execute() {
+		// TODO: Implement this
 	}
 	
 }
