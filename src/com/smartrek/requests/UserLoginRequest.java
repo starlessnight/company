16c6df01f2d5dc4028aba4c4b4b0bb33e70dae82
Sumin Byeon <suminb@gmail.com>
2012-08-30 01:12:36 -0700

    User login is back.

--- src/com/smartrek/requests/UserLoginRequest.java
+++ src/com/smartrek/requests/UserLoginRequest.java
@@ -5,19 +5,18 @@ import java.net.URLEncoder;
 
 import org.json.JSONException;
 
-
 import com.smartrek.models.User;
 
 public class UserLoginRequest extends FetchRequest<User> {
 
 	public UserLoginRequest(String username, String password) {
-//		super(String.format("%s/verifyaccount/username=%s&password=%s",
-//				HOST, URLEncoder.encode(username), URLEncoder.encode(password)));
-	    super("http://static.suminb.com/smartrek/login.html");
+		super(String.format("%s/verifyaccount/username=%s&password=%s",
+				HOST, URLEncoder.encode(username), URLEncoder.encode(password)));
+//	    super("http://static.suminb.com/smartrek/login.html");
 	}
 	
 	public User execute() throws IOException, JSONException {
-		String response = executeFetchRequest(url).trim();
+		String response = executeFetchRequest(getURL()).trim();
 		
 		// Since the server returns a JSON array for no apparent reason...
 		return User.parse(response.substring(1, response.length()-1));
