15f0bf225acf91ff66a661083598ca2e20400629
Sumin Byeon <suminb@gmail.com>
2012-09-05 13:45:35 -0700

    Successfully brings up a context menu on a long press.

--- src/com/smartrek/dialogs/FavoriteAddressListDialog.java
+++ src/com/smartrek/dialogs/FavoriteAddressListDialog.java
@@ -5,7 +5,10 @@ import java.util.List;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -39,6 +42,15 @@ public class FavoriteAddressListDialog extends GenericListDialog<Address> {
 		
 		super.onCreate(savedInstanceState);
 		textViewGeneric.setText("You don't have any favorite address");
+		
+		registerForContextMenu(listViewGeneric);
+	}
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+	    super.onCreateContextMenu(menu, v, menuInfo);
+	    MenuInflater inflater = getOwnerActivity().getMenuInflater();
+	    inflater.inflate(R.menu.context, menu);
 	}
 
 	private class FavoriteAddressListAdapter extends ArrayAdapter<Address> {
