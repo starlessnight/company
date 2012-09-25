a4b14a15ee060753b8695b897712749d9a7018d8
Sumin Byeon <suminb@gmail.com>
2012-09-25 00:38:58 -0700

    I think this is a proper way to use MediaPlayer.

--- src/com/smartrek/activities/ValidationActivity.java
+++ src/com/smartrek/activities/ValidationActivity.java
@@ -105,13 +105,13 @@ public class ValidationActivity extends Activity {
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(false);
         mapView.setOnTouchListener(new OnTouchListener() {
-			
-			@Override
-			public boolean onTouch(View v, MotionEvent event) {
-				buttonFollow.setChecked(false);
-				return false;
-			}
-		});
+            
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                buttonFollow.setChecked(false);
+                return false;
+            }
+        });
         
         MapController mc = mapView.getController();
         mc.setZoom(18);
@@ -140,22 +140,24 @@ public class ValidationActivity extends Activity {
         buttonFollow = (ToggleButton) findViewById(R.id.button_follow);
         buttonFollow.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				if (buttonFollow.isChecked()) {
-					
-				}
-				else {
-					
-				}
-			}
-		});
+            @Override
+            public void onClick(View v) {
+                if (buttonFollow.isChecked()) {
+                    
+                }
+                else {
+                    
+                }
+            }
+        });
         
         ((View) findViewById(R.id.text_view_navigation)).getBackground().setAlpha(220);
 
         if (route.getFirstNode() != null) {
-        	mc.setCenter(route.getFirstNode().getGeoPoint());
+            mc.setCenter(route.getFirstNode().getGeoPoint());
         }
+        
+        preparePingSound();
         
         drawRoute(mapView, route, 0);
         
@@ -169,12 +171,12 @@ public class ValidationActivity extends Activity {
 
         // Register the listener with the Location Manager to receive location updates
         if (debugPrefs.getInt(DebugOptionsActivity.GPS_MODE, DebugOptionsActivity.GPS_MODE_DEFAULT) == DebugOptionsActivity.GPS_MODE_REAL) {
-        	// TODO: Turn on GSP early
-        	locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000, 25, locationListener);
-        	locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 5, locationListener);
+            // TODO: Turn on GSP early
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000, 25, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 5, locationListener);
         }
         else {
-        	fakeLocationService = new FakeLocationService(locationListener);
+            fakeLocationService = new FakeLocationService(locationListener);
         }
 
         startTime = new Time();
@@ -271,67 +273,88 @@ public class ValidationActivity extends Activity {
         new SendTrajectoryTask().execute(seq++, User.getCurrentUser(this).getId());
     }
     
+    private MediaPlayer mediaPlayer;
+    
+    private void preparePingSound() {
+    	Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        mediaPlayer = MediaPlayer.create(ValidationActivity.this, alert);
+        
+        try {
+			mediaPlayer.prepare();
+		}
+		catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+    
     // FIXME: Temporary
     private void playPingSound() {
-        Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        MediaPlayer mp = MediaPlayer.create(this, alert);
-        mp.start();
+//        runOnUiThread(new Runnable() {
+//            public void run() {
+                
+    	mediaPlayer.start();
+                
+//            }
+//        });
     }
     
     private void showNavigationInformation(final Location location, final RouteNode node) {
-    	final double latitude = location.getLatitude();
-    	final double longitude = location.getLongitude();
-    	
-		runOnUiThread(new Runnable() {
-			public void run() {
-				double distance = route.getDistanceToNextTurn(latitude, longitude);
-				double distanceInMile = distance * 0.000621371;
-				double distanceInFoot = distance * 3.28084;
-				
-				String distancePresentation = null;
-				if (distanceInFoot < 1000) {
-					distancePresentation = String.format("%.0f ft", distanceInFoot);
-				}
-				else {
-					distancePresentation = String.format("%.1f mi", distanceInMile);
-				}
-				
-				//String message = String.format("%s in %s", StringUtil.capitalizeFirstLetter(node.getMessage()), distancePresentation);
-				textViewMessage.setText(StringUtil.capitalizeFirstLetter(node.getMessage()));
-				textViewDistance.setText(distancePresentation);
-				textViewRoadname.setText(node.getRoadName());
-				
-				// FIXME: Temporary
-				if (node.hasMetadata()) {
-					RouteNode.Metadata metadata = node.getMetadata();
-				
-					if (!metadata.pingFlags[0] && distanceInFoot <= 500) {
-						metadata.pingFlags[0] = true;
-					    playPingSound();
-					}
-					else if (metadata.pingFlags[1] && distanceInMile <= 1.0) {
-						metadata.pingFlags[1] = true;
-					    playPingSound();
-					}
-					else if (!metadata.pingFlags[2] && distanceInMile <= 2.0) {
-						metadata.pingFlags[2] = true;
-					    playPingSound();
-					}
-				}
-			}
-		});
+        final double latitude = location.getLatitude();
+        final double longitude = location.getLongitude();
+        
+        runOnUiThread(new Runnable() {
+            public void run() {
+                double distance = route.getDistanceToNextTurn(latitude, longitude);
+                double distanceInMile = distance * 0.000621371;
+                double distanceInFoot = distance * 3.28084;
+                
+                String distancePresentation = null;
+                if (distanceInFoot < 1000) {
+                    distancePresentation = String.format("%.0f ft", distanceInFoot);
+                }
+                else {
+                    distancePresentation = String.format("%.1f mi", distanceInMile);
+                }
+                
+                //String message = String.format("%s in %s", StringUtil.capitalizeFirstLetter(node.getMessage()), distancePresentation);
+                textViewMessage.setText(StringUtil.capitalizeFirstLetter(node.getMessage()));
+                textViewDistance.setText(distancePresentation);
+                textViewRoadname.setText(node.getRoadName());
+                
+                // FIXME: Temporary
+                if (node.hasMetadata()) {
+                    RouteNode.Metadata metadata = node.getMetadata();
+                
+                    if (!metadata.pingFlags[0] && distanceInFoot <= 500) {
+                        metadata.pingFlags[0] = true;
+                        playPingSound();
+                    }
+                    else if (metadata.pingFlags[1] && distanceInMile <= 1.0) {
+                        metadata.pingFlags[1] = true;
+                        playPingSound();
+                    }
+                    else if (!metadata.pingFlags[2] && distanceInMile <= 2.0) {
+                        metadata.pingFlags[2] = true;
+                        playPingSound();
+                    }
+                }
+            }
+        });
     }
     
     private synchronized void locationChanged(Location location) {
-    	numberOfLocationChanges += 1;
-    	
-    	trajectory.accumulate(location);
-    	
+        numberOfLocationChanges += 1;
+        
+        trajectory.accumulate(location);
+        
         double lat = location.getLatitude();
         double lng = location.getLongitude();
         
         if (buttonFollow.isChecked()) {
-        	mapView.getController().animateTo(lat, lng);
+            mapView.getController().animateTo(lat, lng);
         }
         
         pointOverlay.setLocation((float)lat, (float)lng);
@@ -340,73 +363,73 @@ public class ValidationActivity extends Activity {
         nearestLink = route.getNearestLink(lat, lng);
         nearestNode = nearestLink.getEndNode();
         
-    	List<RouteNode> routeNodes = route.getNodes();
-    	
-    	ValidationParameters params = ValidationParameters.getInstance();
+        List<RouteNode> routeNodes = route.getNodes();
+        
+        ValidationParameters params = ValidationParameters.getInstance();
         
         double distanceToLink = nearestLink.distanceTo(lat, lng);
         if (distanceToLink <= params.getDistanceThreshold()) {
-        	numberOfInRoute += 1;
+            numberOfInRoute += 1;
         }
-    	
-		if (nearestNode.getFlag() != 0) {
-			showNavigationInformation(location, nearestNode);
-		}
-		else {
-			// find the closest RouteNode with a non-zero flag
-			RouteNode node = nearestNode;
-			while (node.getNextNode() != null) {
-				node = node.getNextNode();
-				if (node.getFlag() != 0) {
-					showNavigationInformation(location, node);
-					break;
-				}
-			}
-		}
-    	
-    	
-    	mapView.postInvalidate();
-    	
-    	if (trajectory.size() >= 8) {
-    		sendTrajectory();
-    	}
+        
+        if (nearestNode.getFlag() != 0) {
+            showNavigationInformation(location, nearestNode);
+        }
+        else {
+            // find the closest RouteNode with a non-zero flag
+            RouteNode node = nearestNode;
+            while (node.getNextNode() != null) {
+                node = node.getNextNode();
+                if (node.getFlag() != 0) {
+                    showNavigationInformation(location, node);
+                    break;
+                }
+            }
+        }
+        
+        
+        mapView.postInvalidate();
+        
+        if (trajectory.size() >= 8) {
+            sendTrajectory();
+        }
         
         if (route.hasArrivedAtDestination(lat, lng)) {
-        	deactivateLocationService();
-        	arriveAtDestination();
-        	Log.d("ValidationActivity", "Arriving at destination");
-        	
-        	try {
-				Log.d("ValidationActivity", "trajectory = " + trajectory.toJSON().toString());
-			}
-        	catch (JSONException e) {
-        		ehs.registerException(e);
-			}
+            deactivateLocationService();
+            arriveAtDestination();
+            Log.d("ValidationActivity", "Arriving at destination");
+            
+            try {
+                Log.d("ValidationActivity", "trajectory = " + trajectory.toJSON().toString());
+            }
+            catch (JSONException e) {
+                ehs.registerException(e);
+            }
         }
     }
     
     private void arriveAtDestination() {
-    	locationManager.removeUpdates(locationListener);
-    	validationTimeoutHandler.removeCallbacks(validationTimeoutNotifier);
-    	
-    	endTime = new Time();
-    	endTime.setToNow();
-    	
-    	sendTrajectory();
-    	
-    	Intent intent = new Intent(this, ValidationReportActivity.class);
-    	intent.putExtra("route", route);
-    	intent.putExtra("numberOfLocationChanges", numberOfLocationChanges);
-    	intent.putExtra("numberOfInRoute", numberOfInRoute);
-    	intent.putExtra("startTime", startTime.toMillis(false));
-    	intent.putExtra("endTime", endTime.toMillis(false));
-    	startActivity(intent);
-    	
-    	finish();
+        locationManager.removeUpdates(locationListener);
+        validationTimeoutHandler.removeCallbacks(validationTimeoutNotifier);
+        
+        endTime = new Time();
+        endTime.setToNow();
+        
+        sendTrajectory();
+        
+        Intent intent = new Intent(this, ValidationReportActivity.class);
+        intent.putExtra("route", route);
+        intent.putExtra("numberOfLocationChanges", numberOfLocationChanges);
+        intent.putExtra("numberOfInRoute", numberOfInRoute);
+        intent.putExtra("startTime", startTime.toMillis(false));
+        intent.putExtra("endTime", endTime.toMillis(false));
+        startActivity(intent);
+        
+        finish();
     }
     
     private void deactivateLocationService() {
-    	fakeLocationService.cancel();
+        fakeLocationService.cancel();
     }
     
     private class ValidationLocationListener implements LocationListener {
@@ -432,11 +455,11 @@ public class ValidationActivity extends Activity {
     private class ValidationTimeoutNotifier extends Thread {
         @Override
         public void run() {
-        	if (endTime == null) {
-        		endTime = new Time();
-        		endTime.setToNow();
-        	}
-        	
+            if (endTime == null) {
+                endTime = new Time();
+                endTime.setToNow();
+            }
+            
             Intent intent = new Intent(ValidationActivity.this, ValidationReportActivity.class);
             intent.putExtra("route", route);
             intent.putExtra("timedout", true);
@@ -454,70 +477,70 @@ public class ValidationActivity extends Activity {
      * Fake data player
      */
     private class FakeLocationService extends TimerTask {
-    	private Timer timer;
-    	private LocationListener listener;
-    	//private Queue<RouteNode> nodes;
-    	
-    	private Queue<GeoPoint> trajectory;
-    	
-    	@SuppressWarnings("unchecked")
-		public FakeLocationService(LocationListener listener) {
-    		this.listener = listener;
-    		
-			try {
-				InputStream in = getResources().getAssets().open("trajectory.csv");
-				trajectory = (Queue<GeoPoint>) PrerecordedTrajectory.read(in);
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-    		timer = new Timer();
-    		timer.schedule(this, 1000, 500);
-    	}
+        private Timer timer;
+        private LocationListener listener;
+        //private Queue<RouteNode> nodes;
+        
+        private Queue<GeoPoint> trajectory;
+        
+        @SuppressWarnings("unchecked")
+        public FakeLocationService(LocationListener listener) {
+            this.listener = listener;
+            
+            try {
+                InputStream in = getResources().getAssets().open("trajectory.csv");
+                trajectory = (Queue<GeoPoint>) PrerecordedTrajectory.read(in);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            timer = new Timer();
+            timer.schedule(this, 1000, 500);
+        }
 
-		@Override
-		public void run() {
-			if (trajectory == null || trajectory.isEmpty()) {
-				timer.cancel();
-			}
-			else {
-				GeoPoint geoPoint = trajectory.poll();
-				Location location = new Location("");
-				location.setLatitude(geoPoint.getLatitude());
-				location.setLongitude(geoPoint.getLongitude());
-				location.setTime(System.currentTimeMillis());
-				listener.onLocationChanged(location);
-			}
-		}
+        @Override
+        public void run() {
+            if (trajectory == null || trajectory.isEmpty()) {
+                timer.cancel();
+            }
+            else {
+                GeoPoint geoPoint = trajectory.poll();
+                Location location = new Location("");
+                location.setLatitude(geoPoint.getLatitude());
+                location.setLongitude(geoPoint.getLongitude());
+                location.setTime(System.currentTimeMillis());
+                listener.onLocationChanged(location);
+            }
+        }
     }
     
     private class SendTrajectoryTask extends AsyncTask<Object, Object, Object> {
 
         private RouteMapper mapper = new RouteMapper();
         
-		@Override
-		protected Object doInBackground(Object... params) {
-		    int seq = (Integer) params[0];
-			int uid = (Integer) params[1];
-			
-			Log.d("ValidationActivity", "sendTrajectory()");
-	    	try {
-				mapper.sendTrajectory(seq, uid, route.getId(), trajectory);
-			}
-	    	catch (Exception e) {
-	    	    ehs.registerException(e);
-			}
-	    	
-			return null;
-		}
-    	
-		@Override
-		protected void onPostExecute(Object result) {
-			// Silently report exceptions
-		    while (ehs.hasExceptions()) {
-		    	System.err.print(ehs.popException());
-		    }
-		}
+        @Override
+        protected Object doInBackground(Object... params) {
+            int seq = (Integer) params[0];
+            int uid = (Integer) params[1];
+            
+            Log.d("ValidationActivity", "sendTrajectory()");
+            try {
+                mapper.sendTrajectory(seq, uid, route.getId(), trajectory);
+            }
+            catch (Exception e) {
+                ehs.registerException(e);
+            }
+            
+            return null;
+        }
+        
+        @Override
+        protected void onPostExecute(Object result) {
+            // Silently report exceptions
+            while (ehs.hasExceptions()) {
+                System.err.print(ehs.popException());
+            }
+        }
     }
 }
