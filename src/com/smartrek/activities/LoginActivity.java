dfb04830ed984a096240cf0618310a14cf3b51a1
yao <yaochunghsu@intumit.com>
2013-02-22 16:12:56 +0800

    new login screen

--- src/com/smartrek/activities/LoginActivity.java
+++ src/com/smartrek/activities/LoginActivity.java
@@ -6,18 +6,25 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Html;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.smartrek.dialogs.NotificationDialog;
 import com.smartrek.models.User;
 import com.smartrek.tasks.LoginTask;
 import com.smartrek.utils.Preferences;
 
-public final class LoginActivity extends Activity implements OnClickListener {
+public final class LoginActivity extends Activity implements OnClickListener,
+        TextWatcher {
 	
 	private EditText editTextUsername;
 	private EditText editTextPassword;
@@ -30,16 +37,16 @@ public final class LoginActivity extends Activity implements OnClickListener {
         /* If it hasn't set up the login screen */
         
         Button login = (Button) findViewById(R.id.login_button);
-        login.setId(1);
         login.setOnClickListener(this);
         
-        Button new_user = (Button) findViewById(R.id.new_user_button);
-        new_user.setId(2);
+        TextView new_user = (TextView) findViewById(R.id.new_user_button);
+        new_user.setText(Html.fromHtml(getString(R.string.create_account_link_text)));
         new_user.setOnClickListener(registerButtonClickListener);
         
         editTextUsername = (EditText) findViewById(R.id.username_box);
+        editTextUsername.addTextChangedListener(this);
         editTextPassword = (EditText) findViewById(R.id.pwd_box);
-       
+        editTextPassword.addTextChangedListener(this);
     }
     
     @Override
@@ -102,5 +109,22 @@ public final class LoginActivity extends Activity implements OnClickListener {
 			startActivity(intent);
 		}
 	};
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count,
+            int after) {
+        
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        Button login = (Button) findViewById(R.id.login_button);
+        login.setEnabled(editTextUsername.getText().length() > 0 && editTextPassword.getText().length() > 0);
+    }
 
 }
