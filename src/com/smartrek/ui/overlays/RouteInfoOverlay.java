325ffa98398160add4fe87ddaf72a99619e1145b
yao <yaochunghsu@intumit.com>
2013-03-07 10:30:37 +0800

    removed unused imports

--- src/com/smartrek/ui/overlays/RouteInfoOverlay.java
+++ src/com/smartrek/ui/overlays/RouteInfoOverlay.java
@@ -10,7 +10,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Paint.Align;
 import android.graphics.Paint.Style;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
