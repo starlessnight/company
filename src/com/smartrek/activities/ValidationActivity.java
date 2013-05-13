d906b389dc842b6495d1b3879bcc9c6c5395f4aa
yao <yaochunghsu@intumit.com>
2013-05-13 17:02:31 +0800

    implemented GPS-fallback-to-network location strategy

--- src/com/smartrek/activities/ValidationActivity.java
+++ src/com/smartrek/activities/ValidationActivity.java
@@ -520,13 +520,12 @@ public final class ValidationActivity extends Activity implements OnInitListener
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
             SystemService.alertNoGPS(this);
-        }
-        else {
+        }else{
             // TODO: Turn on GSP early
-            //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 3000, 25, locationListener);
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 
                 DebugOptionsActivity.getGpsUpdateInterval(this), 5, locationListener);
         }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
 
     public synchronized int[] drawRoute (MapView mapView, Route route, int routeNum) {
@@ -841,10 +840,20 @@ public final class ValidationActivity extends Activity implements OnInitListener
     }
     
     private class ValidationLocationListener implements LocationListener {
+        
+        Location lastLocation;
+        
         public void onLocationChanged(Location location) {
             Log.d(this.getClass().toString(), String.format("onLocationChanged: %s", location));
-            
-            locationChanged(location);
+            SharedPreferences debugPrefs = getSharedPreferences(DebugOptionsActivity.DEBUG_PREFS, MODE_PRIVATE);
+            int gpsMode = debugPrefs.getInt(DebugOptionsActivity.GPS_MODE, DebugOptionsActivity.GPS_MODE_DEFAULT);
+            if (gpsMode == DebugOptionsActivity.GPS_MODE_REAL) {
+                if(isBetterLocation(location, lastLocation)){
+                    locationChanged(location);
+                }
+            }else{
+                locationChanged(location);
+            }
         }
 
         public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -858,6 +867,62 @@ public final class ValidationActivity extends Activity implements OnInitListener
         public void onProviderDisabled(String provider) {
             Log.d(this.getClass().toString(), String.format("onProviderDisabled: %s", provider));
         }
+    }
+    
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+    
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    private static boolean isBetterLocation(Location location, Location currentBestLocation) {
+       if (currentBestLocation == null) {
+           // A new location is always better than no location
+           return true;
+       }
+
+       // Check whether the new location fix is newer or older
+       long timeDelta = location.getTime() - currentBestLocation.getTime();
+       boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+       boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+       boolean isNewer = timeDelta > 0;
+
+       // If it's been more than two minutes since the current location, use the new location
+       // because the user has likely moved
+       if (isSignificantlyNewer) {
+           return true;
+       // If the new location is more than two minutes older, it must be worse
+       } else if (isSignificantlyOlder) {
+           return false;
+       }
+
+       // Check whether the new location fix is more or less accurate
+       int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+       boolean isLessAccurate = accuracyDelta > 0;
+       boolean isMoreAccurate = accuracyDelta < 0;
+       boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+       // Check if the old and new location are from the same provider
+       boolean isFromSameProvider = isSameProvider(location.getProvider(),
+               currentBestLocation.getProvider());
+
+       // Determine location quality using a combination of timeliness and accuracy
+       if (isMoreAccurate) {
+           return true;
+       } else if (isNewer && !isLessAccurate) {
+           return true;
+       } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+           return true;
+       }
+       return false;
+   }
+    
+    /** Checks whether two providers are the same */
+    private static boolean isSameProvider(String provider1, String provider2) {
+        if (provider1 == null) {
+          return provider2 == null;
+        }
+        return provider1.equals(provider2);
     }
     
     private class ValidationTimeoutNotifier extends Thread {
