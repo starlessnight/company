096c5fcbc71ee6dbde144941e05e28225cdcd16a
yao <yaochunghsu@intumit.com>
2013-09-30 10:29:52 +0800

    #237: Landing page is slim and not responsive

--- src/com/smartrek/activities/HomeActivity.java
+++ src/com/smartrek/activities/HomeActivity.java
@@ -271,7 +271,7 @@ public final class HomeActivity extends ActionBarActivity implements TextWatcher
 	        new NotificationTask().execute(User.getCurrentUser(this));
 	        updateAllFavAddrLatLon();
 	        if(Request.NEW_API){
-	            updateDeviceId();
+	            updateDeviceId(); 
 	        }
 	    }
 	    
