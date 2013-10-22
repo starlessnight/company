5549124f35eea231b8af91366c85747449fa348f
yao <yaochunghsu@intumit.com>
2013-10-22 15:10:05 +0800

    fixed the floating menu's layout problem if screen orientation changes

--- src/com/smartrek/dialogs/FloatingMenuDialog.java
+++ src/com/smartrek/dialogs/FloatingMenuDialog.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.smartrek.activities.R;
 import com.smartrek.ui.menu.MainMenu;
@@ -40,17 +42,22 @@ public class FloatingMenuDialog extends Dialog {
 		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		dialogView = (ViewGroup) inflater.inflate(R.layout.floating_menu_dialog, null);
 		
-		int[] menuIds = {R.id.route, R.id.dashboard, R.id.reservation, 
-	        R.id.map_display_options, R.id.home, R.id.logout_option};
+		View content = dialogView.findViewById(R.id.content);
+		Display display = context.getWindowManager().getDefaultDisplay();
+		RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) content.getLayoutParams();
+        layoutParams.width = Math.min(display.getHeight(), display.getWidth());
+		
+        int[] menuIds = {R.id.route, R.id.dashboard, R.id.reservation, 
+            R.id.map_display_options, R.id.home, R.id.logout_option};
 		for(int id : menuIds){
 		    final View vMenu = dialogView.findViewById(id);
 		    vMenu.post(new Runnable() {
-                @Override
-                public void run() {
-                    LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) vMenu.getLayoutParams();
-                    layoutParams.height = vMenu.getWidth() * 5 / 6;
-                }
-            });
+	            @Override
+	            public void run() {
+	                LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) vMenu.getLayoutParams();
+	                layoutParams.height = vMenu.getWidth() * 5 / 6;
+	            }
+	        });
             vMenu.setOnClickListener(new View.OnClickListener() {
 	            @Override
 	            public void onClick(View v) {
