dd332760e1dca3c69909ba17518c407404311b1f
yao <yaochunghsu@intumit.com>
2013-11-07 17:46:27 +0800

    #316 Android regularly crashes in the background

--- src/com/smartrek/activities/GCMIntentService.java
+++ src/com/smartrek/activities/GCMIntentService.java
@@ -35,54 +35,59 @@ public class GCMIntentService extends GCMBaseIntentService {
 
 	@Override
 	protected void onMessage(Context context, Intent intent) {
-		Log.i(LOG_TAG, "GCMIntentService onMessage called");
-		Log.i(LOG_TAG, "Origin: " + intent.getStringExtra("origin"));
-		Log.i(LOG_TAG, "Destination: " + intent.getStringExtra("destination"));
-		Log.i(LOG_TAG, "Time: " + intent.getStringExtra("time"));
-		Log.i(LOG_TAG, "Message: " + intent.getStringExtra("message"));
-		
-		String origin = intent.getStringExtra("origin");
-		String destination = intent.getStringExtra("destination");
-		String message = intent.getStringExtra("message");
-		double lat = Double.parseDouble(intent.getStringExtra("lat"));
-        double lon = Double.parseDouble(intent.getStringExtra("lon"));
-        String eta = intent.getStringExtra("eta");
-        String mile = intent.getStringExtra("mile");
-		
-		long departureTime = intent.getLongExtra("time", 0) * 1000;
-		
-		Intent routeIntent = new Intent(context, RouteActivity.class);
-        routeIntent.putExtra(RouteActivity.ORIGIN_ADDR, origin);
-        routeIntent.putExtra(RouteActivity.DEST_ADDR, destination);
-        
-		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        
-        Notification notification = new Notification(R.drawable.icon_small, "Smartrek", departureTime);
-        PendingIntent pendingIntent;
-        if(eta != null){
-            String msg = message + "\n" + mile + " miles to go estimated arrival time: " + eta;
-            Intent landingIntent = new Intent(context, LandingActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            landingIntent.putExtra(LandingActivity.LAT, lat);
-            landingIntent.putExtra(LandingActivity.LON, lon);
-            landingIntent.putExtra(LandingActivity.MSG, msg);
-            pendingIntent = PendingIntent.getActivity(context, 0, landingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        }else{
-            pendingIntent = PendingIntent.getActivity(context, 0, routeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        }
-        notification.setLatestEventInfo(context, "Smartrek", message, pendingIntent);
-        
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
-        notificationManager.notify(0, notification);
-        
-        PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
-        if(!pm.isScreenOn()){
-           WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP |PowerManager.ON_AFTER_RELEASE, 
-               GCMIntentService.class.getSimpleName() + "Lock");
-           wl.acquire(10000);
-           WakeLock wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
-               GCMIntentService.class.getSimpleName() + "CpuLock");
-           wl_cpu.acquire(10000);
+	    try {
+    		Log.i(LOG_TAG, "GCMIntentService onMessage called");
+    		Log.i(LOG_TAG, "Origin: " + intent.getStringExtra("origin"));
+    		Log.i(LOG_TAG, "Destination: " + intent.getStringExtra("destination"));
+    		Log.i(LOG_TAG, "Time: " + intent.getStringExtra("time"));
+    		Log.i(LOG_TAG, "Message: " + intent.getStringExtra("message"));
+    		
+    		String origin = intent.getStringExtra("origin");
+    		String destination = intent.getStringExtra("destination");
+    		String message = intent.getStringExtra("message");
+    		double lat = Double.parseDouble(intent.getStringExtra("lat"));
+            double lon = Double.parseDouble(intent.getStringExtra("lon"));
+            String eta = intent.getStringExtra("eta");
+            String mile = intent.getStringExtra("mile");
+    		
+    		long departureTime = intent.getLongExtra("time", 0) * 1000;
+    		
+    		Intent routeIntent = new Intent(context, RouteActivity.class);
+            routeIntent.putExtra(RouteActivity.ORIGIN_ADDR, origin);
+            routeIntent.putExtra(RouteActivity.DEST_ADDR, destination);
+            
+    		NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            
+            Notification notification = new Notification(R.drawable.icon_small, "Smartrek", departureTime);
+            PendingIntent pendingIntent;
+            if(eta != null){
+                String msg = message + "\n" + mile + " miles to go estimated arrival time: " + eta;
+                Intent landingIntent = new Intent(context, LandingActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                landingIntent.putExtra(LandingActivity.LAT, lat);
+                landingIntent.putExtra(LandingActivity.LON, lon);
+                landingIntent.putExtra(LandingActivity.MSG, msg);
+                pendingIntent = PendingIntent.getActivity(context, 0, landingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            }else{
+                pendingIntent = PendingIntent.getActivity(context, 0, routeIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            }
+            notification.setLatestEventInfo(context, "Smartrek", message, pendingIntent);
+            
+            notification.flags = Notification.FLAG_AUTO_CANCEL;
+            notificationManager.notify(0, notification);
+            
+            PowerManager pm = (PowerManager)context.getSystemService(Context.POWER_SERVICE);
+            if(!pm.isScreenOn()){
+               WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK |PowerManager.ACQUIRE_CAUSES_WAKEUP |PowerManager.ON_AFTER_RELEASE, 
+                   GCMIntentService.class.getSimpleName() + "Lock");
+               wl.acquire(10000);
+               WakeLock wl_cpu = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                   GCMIntentService.class.getSimpleName() + "CpuLock");
+               wl_cpu.acquire(10000);
+            }
+	    }
+        catch (Throwable t) {
+            Log.d("GCMIntentService", Log.getStackTraceString(t));
         }
 	}
 
