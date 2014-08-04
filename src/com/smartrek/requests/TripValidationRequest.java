3628558fdacd770f3401a35d469e35daef2fb07d
yao <yaochunghsu@intumit.com>
2014-08-04 10:47:14 +0800

    #680

--- src/com/smartrek/requests/TripValidationRequest.java
+++ src/com/smartrek/requests/TripValidationRequest.java
@@ -28,19 +28,15 @@ public class TripValidationRequest extends Request {
 	public void execute(Context ctx) throws Exception {
         Map<String, String> params = new HashMap<String, String>();
         params.put("reservation_id", String.valueOf(rid));
+        Intent intent = new Intent(ValidationActivity.TRIP_VALIDATOR);
         try{
-            Intent intent = new Intent(ValidationActivity.TRIP_VALIDATOR);
-            try{
-                String res = executeHttpRequest(Method.POST, url, params, ctx);
-                JSONObject json = new JSONObject(res);
-                JSONObject data = json.getJSONObject("data");
-                intent.putExtra(ValidationActivity.ID, data.optString("id"));
-                intent.putExtra(ValidationActivity.CREDIT, data.optInt("credit"));
-                intent.putExtra(ValidationActivity.TIME_SAVING_IN_SECOND, data.optInt("time_saving_in_second"));
-                intent.putExtra(ValidationActivity.CO2_SAVING, data.optDouble("co2_saving", 0));
-            } finally{
-                ctx.sendBroadcast(intent);
-            }
+            String res = executeHttpRequest(Method.POST, url, params, ctx);
+            JSONObject json = new JSONObject(res);
+            JSONObject data = json.getJSONObject("data");
+            intent.putExtra(ValidationActivity.ID, data.optString("id"));
+            intent.putExtra(ValidationActivity.CREDIT, data.optInt("credit"));
+            intent.putExtra(ValidationActivity.TIME_SAVING_IN_SECOND, data.optInt("time_saving_in_second"));
+            intent.putExtra(ValidationActivity.CO2_SAVING, data.optDouble("co2_saving", 0));
         }catch(Exception e){
             Log.w("TripValidationRequest", Log.getStackTraceString(e));
             if(responseCode >= 400 && responseCode <= 499){
@@ -48,6 +44,8 @@ public class TripValidationRequest extends Request {
             }else{
                 throw e;
             }
+        }finally{
+            ctx.sendBroadcast(intent);
         }
 	}
 }
