0e6c7f353d6a9a11be31b1f2aeeb723a20e9fc06
larryho <larryho@intumit.com>
2015-01-27 08:05:05 +0800

    #831 Incident display icon-message mismatch

--- src/com/metropia/requests/IncidentRequest.java
+++ src/com/metropia/requests/IncidentRequest.java
@@ -8,13 +8,12 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.metropia.models.User;
 import com.metropia.requests.IncidentRequest.Incident;
 
-public class IncidentRequest extends FetchRequest<List<Incident>>{
-	
+public class IncidentRequest extends FetchRequest<List<Incident>> {
+
 	public static class Incident {
 		public int type;
 		public String shortDesc;
@@ -35,21 +34,22 @@ public class IncidentRequest extends FetchRequest<List<Incident>>{
 		String response;
 		try {
 			response = executeFetchRequest(getUrl(), ctx);
-		}
-		catch(IOException ioe) {
+		} 
+		catch (IOException ioe) {
 			e = ioe;
 			response = ioe.getMessage();
 		}
-		if(e != null) {
+		
+		if (e != null) {
 			throw new Exception(new JSONObject(response).optString(ERROR_MESSAGE));
-		}
+		} 
 		else {
 			JSONArray responseArray = new JSONArray(response);
-			for(int i = 0 ; i < responseArray.length() ; i++) {
+			for (int i = 0; i < responseArray.length(); i++) {
 				JSONObject incidentJSON = responseArray.optJSONObject(i);
-				if("Y".equalsIgnoreCase(incidentJSON.optString("impacting"))) {
+				if ("Y".equalsIgnoreCase(incidentJSON.optString("impacting"))) {
 					Incident incident = new Incident();
-					incident.type = incidentJSON.optInt("type");
+					incident.type = incidentJSON.optInt("metropia_type");
 					incident.lat = incidentJSON.optDouble("latitude");
 					incident.lon = incidentJSON.optDouble("longitude");
 					incident.shortDesc = incidentJSON.optString("ShortDesc");
